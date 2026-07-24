58cdd042d01b1b2d083d2ae52791616c9212e47b
PRAKHAR MISHRA <prakhar.mishra@sap.com>
2026-07-24 19:49:44 +0530

    correction

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import LogoImage from '../../assets/logo.png'; // Ensure logo.png is inside src/assets/
+import LogoImage from '../../assets/logo.png';
 
 function Logo() {
-  return <img src={LogoImage} alt="QTify Logo" width={67} />;
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={LogoImage} alt="QTify Logo" width={67} />
+    </div>
+  );
 }
 
 export default Logo;
